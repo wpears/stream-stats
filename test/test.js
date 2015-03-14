a08a33e1e8133a1dd62c9808e8f7f020aeb2dfe4
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-13 20:55:58 -0400

    test different lengths

--- test/test.js
+++ test/test.js
@@ -1,33 +1,37 @@
-var test = require('tape');
-
-var stats = require('../index.js');
 var fs = require('fs');
+var test = require('tape');
+var stats = require('../index.js');
 
 
 function lengthCheck(t, statObj){
   return function(err, data){
     if(err) throw new Error('Error testing output file size');
-    t.equal(statObj.byteCount, data.length);  
+    t.equal(statObj.byteCount, data.length, 'Byte count is accurate');  
   }
 }
 
+function testChunksByLength(len){
+  test(len+' byte chunks', function(t){
+    t.plan(1);
 
-test('512 byte chunks', function(t){
-  t.plan(2);
+    var input = 'test/data/preludes.txt'
+    var output = 'test/data/'+ Math.random() + '.txt';
+    var testName = len + ' length chunk';
+    var midStats = stats(testName)
+     
+    fs.createReadStream(input, {highWaterMark:len})
+      .pipe(midStats)
+      .pipe(fs.createWriteStream(output))
 
-  var input = 'test/data/preludes.txt'
-  var output = 'test/data/precop.txt';
-
-  fs.createReadStream(input, {highWaterMark:512})
-    .pipe(stats('Buffer test'))
-    .pipe(fs.createWriteStream(output))
-    .on('close',function(){
-      var statObj = stats.getResults('Buffer test');
-
-      t.equal(statObj.chunkCount, 4, 'Got a result');
+    midStats.on('end',function(){
+      var statObj = stats.getResults(testName);
 
       fs.readFile(input, lengthCheck(t, statObj));  
+      
+      fs.unlinkSync(output);   
+    });
+  });
+}
 
-      fs.unlinkSync(output);
-   });
-});
+testChunksByLength(512);
+testChunksByLength(1);
